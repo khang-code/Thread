50c4da74472d092d20e738e18ad1c5a66cea32ab
khang-code <trieukhang2006@gmail.com>
2024-09-27 09:24:18 +0700

    Add files via upload

---
+++ Thread/src/com/codegym/runable/RunnableDemo.java
@@ -0,0 +1,30 @@
+package com.codegym.runable;
+
+public class RunnableDemo implements Runnable {
+    private Thread t;
+    private final String threadName;
+    public RunnableDemo (String name){
+        threadName = name;
+        System.out.println("Creating " + threadName);
+    }
+    @Override
+    public void run() {
+        try {for (int i = 4; i > 0; i--) {
+            System.out.println("Thread: " + threadName + ", " + i);
+            // Dừng vòng lặp trong 50 ms
+            Thread.sleep(50);
+        }
+    } catch (InterruptedException e) {
+        System.out.println("Thread " + threadName + " interrupted.");
+    }
+        System.out.println("Thread " + threadName + " exiting.");
+}
+
+    public void start() {
+        System.out.println("Starting " + threadName);
+        if (t == null) {
+            t = new Thread(this,threadName);
+            t.start();
+        }
+    }
+}
